58a057d2daa706c109fec09fefb0f0473af830be
Nicolas <nicolas.mohr@riskident.com>
2022-12-05 16:30:56 +0100

    added day02 solution

---
+++ src/day02.rs
@@ -0,0 +1,129 @@
+use std::{error::Error, fs};
+
+type Result<T> = std::result::Result<T, Box<dyn Error>>;
+
+pub fn part1() -> Result<i32> {
+    let input = fs::read_to_string("day02.input")?;
+    let lines: Vec<_> = input.lines().collect();
+    let sum = lines
+        .iter()
+        .map(|line| Match::try_from_str(line).unwrap())
+        .map(|mtch| mtch.get_player_score())
+        .sum();
+    Ok(sum)
+}
+
+pub fn part2() -> Result<i32> {
+    let input = fs::read_to_string("day02.input")?;
+    let lines: Vec<_> = input.lines().collect();
+    let sum = lines
+        .iter()
+        .map(|line| Match::try_from_str_part2(line).unwrap())
+        .map(|mtch| mtch.get_player_score())
+        .sum();
+    Ok(sum)
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum Move {
+    Rock = 1,
+    Paper = 2,
+    Scissors = 3,
+}
+
+impl Move {
+    fn try_from_char_enemy(c: char) -> Result<Self> {
+        match c {
+            'A' => Ok(Self::Rock),
+            'B' => Ok(Self::Paper),
+            'C' => Ok(Self::Scissors),
+            _ => Err("character has no corresponding enum variant".into()),
+        }
+    }
+    fn try_from_char_player(c: char) -> Result<Self> {
+        match c {
+            'X' => Ok(Self::Rock),
+            'Y' => Ok(Self::Paper),
+            'Z' => Ok(Self::Scissors),
+            _ => Err("character has no corresponding enum variant".into()),
+        }
+    }
+}
+
+/// Describes the Result of a rock, paper, scissors game from the player perspective
+enum MatchResult {
+    Loss = 0,
+    Draw = 3,
+    Win = 6,
+}
+
+impl MatchResult {
+    fn try_from_char(c: char) -> Result<Self> {
+        match c {
+            'X' => Ok(Self::Loss),
+            'Y' => Ok(Self::Draw),
+            'Z' => Ok(Self::Win),
+            _ => Err("character has no corresponding enum variant".into()),
+        }
+    }
+}
+
+struct Match {
+    player: Move,
+    enemy: Move,
+}
+
+impl Match {
+    fn new(player: Move, enemy: Move) -> Self {
+        Self { player, enemy }
+    }
+
+    fn from_enemy_and_result(enemy: Move, desired_result: MatchResult) -> Self {
+        let player = match (enemy, desired_result) {
+            (Move::Rock, MatchResult::Win) => Move::Paper,
+            (Move::Rock, MatchResult::Loss) => Move::Scissors,
+            (Move::Paper, MatchResult::Win) => Move::Scissors,
+            (Move::Paper, MatchResult::Loss) => Move::Rock,
+            (Move::Scissors, MatchResult::Win) => Move::Rock,
+            (Move::Scissors, MatchResult::Loss) => Move::Paper,
+            (e, MatchResult::Draw) => e,
+        };
+        Self { player, enemy }
+    }
+
+    fn try_from_str(s: &str) -> Result<Self> {
+        let enemy_char: char = s.chars().nth(0).unwrap();
+        // .ok_or(Err("Failed to find 0th element in line".into()))?;
+        let enemy_move = Move::try_from_char_enemy(enemy_char)?;
+
+        let player_char: char = s.chars().nth(2).unwrap();
+        // .ok_or(Err("Failed to find second element in line".into()))?;
+        let player_move = Move::try_from_char_player(player_char)?;
+        Ok(Self {
+            player: player_move,
+            enemy: enemy_move,
+        })
+    }
+
+    fn try_from_str_part2(s: &str) -> Result<Self> {
+        let enemy_char = s.chars().nth(0).unwrap();
+        let enemy_move = Move::try_from_char_enemy(enemy_char)?;
+        let desired_result_char = s.chars().nth(2).unwrap();
+        let desired_result = MatchResult::try_from_char(desired_result_char)?;
+        Ok(Self::from_enemy_and_result(enemy_move, desired_result))
+    }
+
+    fn get_match_result(&self) -> MatchResult {
+        match (self.player, self.enemy) {
+            (Move::Scissors, Move::Paper) => MatchResult::Win,
+            (Move::Rock, Move::Scissors) => MatchResult::Win,
+            (Move::Paper, Move::Rock) => MatchResult::Win,
+            (a, b) if a == b => MatchResult::Draw,
+            _ => MatchResult::Loss,
+        }
+    }
+
+    fn get_player_score(&self) -> i32 {
+        self.player as i32 + self.get_match_result() as i32
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::error::Error;
 
 mod day01;
+mod day02;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let part1_result = day01::part1()?;
+    let part1_result = day02::part1()?;
     println!("part 1 result: {}", part1_result);
-    let part2_result = day01::part2()?;
+    let part2_result = day02::part2()?;
     println!("part 2 result: {}", part2_result);
     Ok(())
 }
