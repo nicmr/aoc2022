fe070d391dba01875af34579ec3e808391a1f4cf
Nicolas <nicolas.mohr@riskident.com>
2022-12-23 10:52:53 +0100

    Calling main will now run specified tests

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use std::{env, process::Command, error::Error};
 
 type BoxedError = Box<dyn Error>;
 type Result<T> = std::result::Result<T, BoxedError>;
@@ -10,5 +10,13 @@ pub mod day04;
 // pub mod day10;
 
 fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let day_arg = args.get(1).ok_or("Missing argument. The argument should be the day")?;
+    let cmd = Command::new("cargo")
+        .arg("test")
+        .arg(day_arg)
+        .args(["--", "--nocapture"])
+        .output()?;
+    println!("{}", String::from_utf8_lossy(&cmd.stdout));
     Ok(())
 }
